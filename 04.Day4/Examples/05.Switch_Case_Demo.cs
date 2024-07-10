d155b2fdc47b3d57fe133b4712ffb2a753097e3f
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2024-07-10 12:53:03 +0530

    Add files via upload

---
+++ 04.Day4/Examples/05.Switch_Case_Demo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SALES_DEPARTMENT = 10;
+            const int ADMIN_DEPARTMENT = 20;
+            const int ACCOUNTS_DEPARTMENT = 30;
+            const int OPERATIONS_DEPARTMENT = 40;
+
+
+            Console.WriteLine("Enter your deptno : ");
+            int dno = int.Parse(Console.ReadLine());
+
+
+            switch(dno)
+            {
+                case SALES_DEPARTMENT:
+                    Console.WriteLine("You are belongs to Sales department");
+                    break;
+
+                case ADMIN_DEPARTMENT:
+                    Console.WriteLine("You are belongs to Admin department");
+                    break;
+
+                case ACCOUNTS_DEPARTMENT:
+                    Console.WriteLine("You are belongs to Accounts department");
+                    break;
+
+                case OPERATIONS_DEPARTMENT:
+                    Console.WriteLine("You are belongs to Operations department");
+                    break;
+
+                default:
+                    Console.WriteLine("Inavalid dept number");
+                    break;
+
+            }
+             
+         
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 04.Day4/Examples/06.Loops_Usage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+
+            i = 1;
+            while( i <= 20)
+            {
+                Console.Write("{0}  ",i);
+                i++;
+            }
+
+            Console.WriteLine("\n-----------------------------");
+
+            i = 0;
+            do
+            {
+                Console.Write("{0}  ", i);
+                i = i + 2;              
+            } while (i <= 50);
+
+            Console.WriteLine("\n-----------------------------");
+
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.Write("{0}  ", j);
+            }
+
+            Console.WriteLine("\n-----------------------------");
+
+            // 50  45  40 35 30 .............  5
+
+            for (int j = 50; j >= 5; j = j-5)
+            {
+                Console.Write("{0}  ", j);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
