9d7f97ebbf275cef6506490fccc5c0d8bde15c4b
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2024-07-10 10:46:48 +0530

    Add files via upload

---
+++ 04.Day4/Examples/02.Eg2_Multiple_IfElse_blocks.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SALES_DEPARTMENT = 10;
+            const int ADMIN_DEPARTMENT = 20;
+            const int ACCOUNTS_DEPARTMENT = 30;
+            const int OPERATIONS_DEPARTMENT = 40;
+
+
+            Console.WriteLine("Enter your deptno : ");
+            int dno = int.Parse(Console.ReadLine());
+
+            if (dno == SALES_DEPARTMENT)
+            {
+                Console.WriteLine("You are belongs to Sales department");
+            }  
+            else if (dno == ADMIN_DEPARTMENT)
+            {
+                Console.WriteLine("You are belongs to Admin department");
+            }
+            else if (dno == ACCOUNTS_DEPARTMENT)
+            {
+                Console.WriteLine("You are belongs to Accounts department");
+            }
+            else if (dno == OPERATIONS_DEPARTMENT)
+            {
+                Console.WriteLine("You are belongs to Operations department");
+            }
+            else
+            {
+                Console.WriteLine("Inavalid dept number");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
