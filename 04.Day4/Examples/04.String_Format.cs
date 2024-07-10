46c3b6204b72a5bcc5decbe2234d31dcb7952e2e
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2024-07-10 12:53:34 +0530

    Add files via upload

---
+++ 04.Day4/Examples/04.String_Format.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int empid = 546452;
+            string ename = "Narasimha";
+            string job = "Manager";
+
+            Console.WriteLine("Employee Details :  " + empid + "," +  ename + "," +  job);
+
+            Console.WriteLine("Employee Details :  {0},{1},{2}", empid, ename, job);
+
+            string output = string.Format("Employee Details :  {0},{1},{2}", empid, ename, job);
+            Console.WriteLine(output);
+
+
+            Console.ReadLine();
+        }
+    }
+}
