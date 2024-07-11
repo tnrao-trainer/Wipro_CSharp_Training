55b9732cc9c0b873fea167ab81bb6ade41591fec
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2024-07-11 11:11:54 +0530

    Add files via upload

---
+++ 05.Day5/Examples/01.Eg1_Functions_Demo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Hello()
+        {
+            Console.WriteLine("Hi, Good Morning..!");
+        }
+
+        static void Greeting(string uname)
+        {
+            Console.WriteLine("Hi {0}, How are you?", uname);
+        }
+
+        static int Sum(int x, int y)
+        {
+            return x+y;
+        }
+
+        static string GetCurrentUserName()
+        {
+            return "Scott";
+        }
+
+        static void Main(string[] args)
+        {         
+            Hello();
+            Greeting("Smith");
+
+            int n = Sum(10,20);
+            Console.WriteLine("Sum Result : " + n);
+
+            string str = GetCurrentUserName();
+            Console.WriteLine("GetCurrentUserName result : " +  str);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 05.Day5/Examples/02.Eg2_IsValidUser_Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static  bool IsValidUser(string userId, string password)
+        {
+            if (userId.Trim() == "admin" && password.Trim() == "admin123")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int count = 1;
+
+            while (count <= 3)
+            {
+                Console.WriteLine("Enter User Id : ");
+                string userId = Console.ReadLine();
+
+                Console.WriteLine("Enter Password : ");
+                string password = Console.ReadLine();
+
+
+                bool result = IsValidUser(userId, password);
+
+
+                if (result)
+                {
+                    Console.WriteLine("Welcome to {0}", userId);
+                    break;
+                }                    
+                else
+                {
+                  
+                    Console.WriteLine("Invalid User Id or Password. Wrong Attempts Count : " + count );
+                    
+                    if(count == 3)
+                        Console.WriteLine("Your account is locked.");
+                    
+                    count++;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
