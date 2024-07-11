2c02b4c752e6d8f57ec718cde4fbbcb3d95ea1ac
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2024-07-11 18:19:23 +0530

    Add files via upload

---
+++ 05.Day5/Examples/03.Eg3_Factorial_without_Recursion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+
+        static int  Factorial(int n)
+        {
+            int fact = 1;
+
+            for(int i = 1; i <=n; i++)
+            {
+                fact = fact * i;
+            }
+
+            return fact;
+        }
+ 
+        static void Main(string[] args)
+        {
+            Console.WriteLine( Factorial(5));
+            Console.WriteLine( Factorial(6));
+
+            Console.ReadLine();           
+        }
+    }
+}
---
+++ 05.Day5/Examples/04.Eg4_Factorial_with_Recursion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+
+        static int  Factorial(int n)
+        {
+          //  Console.WriteLine(n);
+          //  Console.ReadLine();
+
+            if (n == 1)
+                return 1;
+            else 
+                return n * Factorial(n - 1);
+        }
+ 
+        static void Main(string[] args)
+        {
+            Console.WriteLine( Factorial(5));
+           
+
+            Console.ReadLine();           
+        }
+    }
+}
