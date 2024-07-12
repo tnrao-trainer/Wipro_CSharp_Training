f66b80f5e9d1e94fe79ab2a1ae7b0d4789ba21f2
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2024-07-12 10:58:01 +0530

    Add files via upload

---
+++ 06.Day6/Program_Eg3_Array_Methods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+ 
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 55,10,15,18,26,14, 63, 11 };
+
+
+            foreach (int item in arr)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.WriteLine();
+
+
+            Array.Reverse(arr);
+            foreach (int item in arr)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            foreach (int item in arr)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+
+
+            Array.Clear(arr,0,arr.Length);
+            foreach (int item in arr)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 06.Day6/Program_Eg5_Array_Searching_IndexOf.cs
@@ -0,0 +1,42 @@
+﻿ using System;
+using System.Runtime.Remoting.Services;
+
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void PrintArray(int[] arr)
+        {
+            foreach (int item in arr)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 55,10,15,18,26,14, 63, 11 };
+ 
+            PrintArray(arr);
+
+            Console.WriteLine("Enter element to search : ");
+            int n = int.Parse(Console.ReadLine());  
+
+            int position = Array.IndexOf(arr, n);
+
+            if (position == -1)
+            {
+                Console.WriteLine("Element does not exists in the array");
+            }
+            else
+            {
+                Console.WriteLine("Yes. Element exists in the array");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
