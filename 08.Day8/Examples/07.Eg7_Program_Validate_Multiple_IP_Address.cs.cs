9c9f15e092f0d1737701586f9670cc0b775ea65b
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2024-07-16 17:58:34 +0530

    Add files via upload

---
+++ 08.Day8/Examples/07.Eg7_Program_Validate_Multiple_IP_Address.cs.cs
@@ -0,0 +1,63 @@
+﻿// Program to validate the given IP Address
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static bool IsValidIPAddress(string ipAddress)
+        {
+            bool isValid = true;
+
+            // 1. Four Parts
+            // 2. Each part separate with.
+            string[] ipParts = ipAddress.Split('.');
+
+            if (ipParts.Length != 4)
+            {
+                isValid = false;
+                return isValid;
+            }
+
+            foreach (string ipPart in ipParts)
+            {
+                int n = -1;
+
+                //3.  Each part should be a number
+                isValid = int.TryParse(ipPart, out n);
+
+                if (isValid == false)
+                {
+                    break;
+                }
+
+                // 4.Each part value  should be in between  0 to 255
+                if (n < 0 || n > 255)
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            return isValid;
+
+        }
+
+        static void Main(string[] args)
+        {
+            string[] ipAddressArray = { "192.168.0.1", "317.172.224.47", "192.168.100.1ab",  "158.85.65", "172.16.254.1" };
+
+            foreach (string ipAddress in ipAddressArray) 
+            {
+                bool isValid = IsValidIPAddress(ipAddress);
+                Console.WriteLine("{0} - {1}", ipAddress, isValid);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 08.Day8/Examples/08.Eg8_Program_Merging_TwoArrays_Third.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+
+            int[] A = { 10, 15, 19, 45,78, 120 };
+            int[] B = { 5, 11, 17, 20, 35, 120 };
+
+            int[] C = new int[A.Length + B.Length];
+
+
+            int i = 0, j = 0, k = 0;
+
+            while (i < A.Length && j < B.Length)
+            {
+                if (A[i] < B[j])
+                {
+                    C[k] = A[i];
+                    k++;
+                    i++;
+                }
+                else if (A[i] > B[j])
+                {
+                    C[k] = B[j];
+                    k++;
+                    j++;
+                }
+                else
+                {
+                    C[k] = A[i];
+                    k++;
+                    i++;
+                    j++;
+                }
+            }
+
+            // copy remaining items from A to C
+            while(i < A.Length)
+            {
+                C[k] = A[i];
+                k++;
+                i++;
+            }
+
+            // copy remaining items from B to C
+            while (j < B.Length)
+            {
+                C[k] = B[j];
+                k++;
+                j++;
+            }
+
+            for (int n = 0; n < k; n++)
+            {
+                Console.Write("  " + C[n]);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
