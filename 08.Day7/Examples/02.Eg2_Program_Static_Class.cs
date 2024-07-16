ab817fa6bd17754dddf203c9d4704a215ba53b53
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2024-07-16 13:01:34 +0530

    Add files via upload

---
+++ 08.Day7/Examples/02.Eg2_Program_Static_Class.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    static class TimeUtil
+    {
+        public static void PrintTime()
+        {
+           // Console.WriteLine(DateTime.Now.ToString("t"));    // HH:MM
+            Console.WriteLine(DateTime.Now.ToString("T"));    // HH:MM:SS
+        }
+        public static void PrintDate()
+        {
+            Console.WriteLine(DateTime.Now.ToString("d"));
+        }
+        public static void PrintDateAndTime()
+        {
+            Console.WriteLine(DateTime.Now.ToString());
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeUtil.PrintTime();
+            TimeUtil.PrintDate();
+            TimeUtil.PrintDateAndTime();
+
+           //   TimeUtil obj = new TimeUtil(); 
+           //  Error :  Cannot create instance of the static class 
+
+
+            Console.ReadLine();
+        }
+    }
+}
